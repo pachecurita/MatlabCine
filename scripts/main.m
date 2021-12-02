b9c62d0a57a43f028c6133821a1c6f956c3550a2
pachecurita <paula.huichacura@usach.cl>
2021-12-02 20:07:30 -0300

    Carpetas del proyecto

--- scripts/main.m
+++ scripts/main.m
@@ -2,6 +2,7 @@
 % Este programa se encarga de gestionar las reservas de un cine que tiene 5
 % salas, donde cada una de ellas tiene 25 filas y 20 columnas para la
 % distribución de las sillas.
+
 clear;close;clc;
 filename = 'datosCine.mat';
 
